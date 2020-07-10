2a3f8b96059a17c22395e6059b056903b88a2d01
Ben Lengerich <blengerich@gmail.com>
2020-07-10 16:46:50 -0400

    add estimates of daily incidence

--- date_utils.py
+++ date_utils.py
@@ -4,3 +4,11 @@ def readable_date(date):
 
 def get_date_format(day, month, year):
     return year*1e4 + month*1e2 + day
+
+
+dates = [get_date_format(d, 3, 2020) for d in range(16, 32)] # March dates (before the 16th many states did not report negative test counts)
+dates.extend([get_date_format(d, 4, 2020) for d in range(1, 31)]) # April dates
+dates.extend([get_date_format(d, 5, 2020) for d in range(1, 32)]) # May dates
+dates.extend([get_date_format(d, 6, 2020) for d in range(1, 31)]) # June dates
+dates.extend([get_date_format(d, 7, 2020) for d in range(1, 10)]) # July dates
+readable_dates = list(map(readable_date, dates))
